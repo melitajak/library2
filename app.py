24ad134d352d2e7b31b957c589d5800bd264760f
melitajak <127343818+melitajak@users.noreply.github.com>
2024-04-24 01:16:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -259,4 +259,4 @@ def unassign_library(book_id):
     return jsonify({'error': 'Book not found'}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
